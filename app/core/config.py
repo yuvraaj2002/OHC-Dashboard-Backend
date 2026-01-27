c9afceb223b9cd3db8ec09168ead33b57a4dc7ae
yuvraaj2002 <ys2002github@gmail.com>
2026-01-27 18:37:36 +0000

    Added guinicorn and also added ignore extra env file variables

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,8 @@ class Settings(BaseSettings):
     
     model_config = SettingsConfigDict(
         env_file=".env",
-        case_sensitive=True
+        case_sensitive=True,
+        extra="ignore"
     )
         
 settings = Settings()
